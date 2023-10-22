6fc69d7937625c45df275fd2d87116a584d858a7
knutdF <45771757+knutdF@users.noreply.github.com>
2023-10-22 23:13:33 +0200

    Update User.kt

--- src/main/kotlin/com/trainingsapp/trainigsapp/model/User.kt
+++ src/main/kotlin/com/trainingsapp/trainigsapp/model/User.kt
@@ -2,11 +2,12 @@ package com.trainingsapp.trainigsapp.model
 
 data class User(
     val id: String,
+    val email: String?,
     val username: String,
-    val password: String, // In Produktionsumgebungen das Passwort niemals im Klartext speichern!
-    val userType: UserType,
-    val departmentId: String? // nullable, falls ein User keinem Department zugeordnet ist
-)
+    val password: String // In Produktionsumgebungen das Passwort niemals im Klartext speichern!){}
+) {
+
+}
 
 enum class UserType {
     ADMIN, USER
